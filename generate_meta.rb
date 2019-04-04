21015950cd76e1428080e7c700293d5139e66645
fbsender <lmctrello@gmail.com>
2019-04-04 20:43:59 +0800

    update

--- generate_meta.rb
+++ generate_meta.rb
@@ -8,6 +8,8 @@ dir = Pathname.new(File.dirname(__FILE__)).realpath
 
 meta = {}
 articles = []
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true)
+
 
 Dir.foreach(dir) do |file|
   if file.match(/\.sac$/)
@@ -15,6 +17,9 @@ Dir.foreach(dir) do |file|
     content = f.split("######")
     yaml = YAML.load(content[0])
     articles << {id: yaml[:article][:id], title: yaml[:article][:title], file: file}
+    File.open("#{yaml[:article][:id]}.html", 'w') do|f|
+      f.puts(markdown.render(content[1]))
+    end
   end
 end
 
